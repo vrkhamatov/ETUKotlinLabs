80a43a7cdc8799cc469df3f286e53f6ffad95f9e
Vadim Khamatov <74698624+vrkhamatov@users.noreply.github.com>
2022-11-07 01:53:11 +0300

    make correct reading XML file and search NOTUNIQUE elements

--- src/main/kotlin/test.kt
+++ src/main/kotlin/test.kt
@@ -1,14 +1,15 @@
-package CsvXmlParser
+import java.util.*
+import kotlin.collections.HashMap
+
 
 class Address
     (
-        val city : String = "",
+    val city : String = "",
     val street : String = "",
     val house : Int = 0,
     val floor : Int = 0
-    )
+)
 {
-
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
@@ -36,4 +37,18 @@ class Address
     }
 
 
+}
+
+fun main(){
+    var map : HashMap<Address,Int>  = hashMapOf()
+    var i : Int
+    var add = Address()
+    var k = 0
+    for (i in 1..4) {
+        k = k+1
+        add = Address("Vadim", "Man", 11, 11)
+        map.put(add, k)
+        println(map.size)
+    }
+    println(map)
 }
